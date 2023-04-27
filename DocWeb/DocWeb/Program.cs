9c66a3cdec2693f541c906bc0683c60d3708482a
JirkaKalvoda <JirkaKalvoda@outlook.com>
2023-04-27 15:03:59 +0800

    添加单实例启动

--- DocWeb/DocWeb/Program.cs
+++ DocWeb/DocWeb/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +15,12 @@ namespace DocWeb
         [STAThread]
         static void Main()
         {
+            // 单实例启动
+            Mutex mutex = new Mutex(false, "DocWeb_{E89B2FE4-C841-4065-AA7F-C7A31F724D0F}", out bool createdNew);
+            if (!createdNew)
+            {
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
